050b894e7ad228464b4389cac294954a472d64b6
ssraminder <ss.raminder@gmail.com>
2025-09-13 22:50:01 -0600

    style: apply Cethos branding

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import LandingPage from './LandingPage';
+import './src/styles/cethos.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
